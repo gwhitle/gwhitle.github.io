2091fa1ec48dcb1a488b76546b3d99fa665b03b2
gwhitle <142765243+gwhitle@users.noreply.github.com>
2024-10-07 01:15:11 -0400

    updated php changes
    updated php changes in link to old_index.html

--- web250/contents/footer.php
+++ web250/contents/footer.php
@@ -2,7 +2,7 @@
     <nav>
         <a href="https://github.com/gwhitle">GitHub</a>
         <a href="https://gwhitle.github.io/">GitHub.io</a>
-        <a href="https://gwhitle.github.io/web250">WEB250.io</a>
+        <a href="https://gwhitle.github.io/web250/old_index.html">WEB250.io</a>
         <a href="https://www.freecodecamp.org/gwhitle">freeCodeCamp</a>
         <a href="https://www.codecademy.com/profiles/gwhitle">Codecademy</a>
         <a href="https://jsfiddle.net/u/gwhitle/fiddles/">JSFiddle</a>
