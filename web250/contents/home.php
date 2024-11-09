198e8a6015263d122cc8703297735fac48c9fc0d
gwhitle <142765243+gwhitle@users.noreply.github.com>
2024-11-09 02:32:14 -0500

    PHP: Updated paragraph and link
    - Updated paragraph and link in PHP hosted site (InfinityFree) to match current HTML page

--- web250/contents/home.php
+++ web250/contents/home.php
@@ -2,9 +2,8 @@
         <h2>Home</h2>
         <p>This is the homepage for Gina W's WEB250 coursework.</p>
         <p>Here you will find links to all projects for WEB250 allowing you to follow my work over this course!</p>
-        <p>You can explore my work from the navigation bar above, or jump right to my introduction by <a
-                href="?p=introduction">clicking this
-                link</a></p>
+        <p>You can explore my work from the navigation bar above, or <a href="introduction.html">go straight to my
+                introduction here</a></p>
         <ul>
         <li>Super Duper Paratrooper - <a href="/multipage_sites/superduper_static/index.htm"
                     target="_blank">Static Page</a></li>
